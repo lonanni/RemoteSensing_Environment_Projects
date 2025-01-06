794df0cd15b8a1e79cab013f3d86cab11e466146
lonanni <lorenza.nanni@port.ac.uk>
2025-01-06 11:43:16 +0000

    adding functions to sediment utils

--- WaterNets_project/sediment_utils.py
+++ WaterNets_project/sediment_utils.py
@@ -12,3 +12,12 @@ class SimpleScaler:
     def fit_transform(self, X):
         self.fit(X)
         return self.transform(X)
+        
+#This scales the image colours so that it looks nice
+def scale_im(reader):
+
+    red = reader.read(3)
+    green = reader.read(2)
+    blue = reader.read(1)
+    scale = lambda x : (x*0.0000275) - 0.2
+    return np.dstack([scale(red), scale(green), scale(blue)]) * 5
